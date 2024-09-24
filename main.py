7566e03182ebe6ae23fcff9a0b842609607a9032
Sarthak <sathakjariwala@outlook.com>
2024-09-24 13:15:10 -0400

    fifth commit-> mappings removed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from capturing_tag_data import capture_tag_data
 def main():
     list_of_logs_obtained = capture_flow_log_data()
     dictionary_containing_tags = capture_tag_data()
-    tags_output, protocols_output = core_logic.process_logs_and_count_tags(list_of_logs_obtained, dictionary_containing_tags, tag_mappings)
+    tags_output, protocols_output = core_logic.process_logs_and_count_tags(list_of_logs_obtained, dictionary_containing_tags)
     
     print("---------------------final_Print-----------------------")
     print(tags_output)
